e7514c8d28658f080a2fbb02d5bb8f2a4fff69f5
Mila289 <mila.krasulina@gmail.com>
2024-11-21 21:15:05 +0700

    Добавляю главное окно

--- Reminder.py
+++ Reminder.py
@@ -1,6 +1,16 @@
-from tkinter import
+from tkinter import TkVersion, Button
 from tkinter import messagebox as mb
 from tkinter import simpledialog as sd
 import datetime
 import time
 import pygame
+
+
+window = Tk
+window.title ("Напоминание")
+label = Label(text="Установите напоминание", font="Arial", 14)
+label.pack(pady=10)
+set_button = Button(text="Установить напоминание", command=set)
+set_button.pack(pady=10)
+
+window.mainloop()
