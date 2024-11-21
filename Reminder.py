d3fbff464e18d1e15f79df60f60330c540790dde
Mila289 <mila.krasulina@gmail.com>
2024-11-21 22:02:32 +0700

    Добавила функцию включения музыки.

--- Reminder.py
+++ Reminder.py
@@ -35,6 +35,15 @@ def check():
         window.after(10000, check())
 
 
+def play_snd():
+    global.music
+    pygame.mixer.init()
+    pygame.mixer.music.load("reminder.mp3")
+    pygame.mixer.music.play()
+
+
+
+
 
 window = Tk
 window.title ("Напоминание")
